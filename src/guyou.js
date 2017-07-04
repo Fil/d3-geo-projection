fd9f4a4be49ae61ccad272d3413fe6acd3caa5dd
Philippe Rivière <fil@rezo.net>
2017-07-04 11:41:42 +0200

    use complex

--- src/guyou.js
+++ src/guyou.js
@@ -1,5 +1,6 @@
 import {geoProjection as projection} from "d3-geo";
 import {ellipticF, ellipticFi, ellipticJi} from "./elliptic";
+import {complexAtan, complexDivide} from "./complex";
 import {abs, atan, atan2, cos, exp, halfPi, log, pi, sin, sqrt, sqrt2, tan} from "./math";
 import squareRaw from "./square";
 
@@ -10,27 +11,9 @@ export function guyouRaw(lambda, phi) {
       f = -1,
       psi = log(tan(pi / 4 + abs(phi) / 2)),
       r = exp(f * psi) / sqrt(k_),
-      at = guyouComplexAtan(r * cos(f * lambda), r * sin(f * lambda)),
+      at = complexAtan(r * cos(f * lambda), r * sin(f * lambda)),
       t = ellipticFi(at[0], at[1], k * k);
   return [-t[1], (phi >= 0 ? 1 : -1) * (0.5 * K - t[0])];
-}
-
-function guyouComplexAtan(x, y) {
-  var x2 = x * x,
-      y_1 = y + 1,
-      t = 1 - x2 - y * y;
-  return [
-   0.5 * ((x >= 0 ? halfPi : -halfPi) - atan2(t, 2 * x)),
-    -0.25 * log(t * t + 4 * x2) +0.5 * log(y_1 * y_1 + x2)
-  ];
-}
-
-function guyouComplexDivide(a, b) {
-  var denominator = b[0] * b[0] + b[1] * b[1];
-  return [
-    (a[0] * b[0] + a[1] * b[1]) / denominator,
-    (a[1] * b[0] - a[0] * b[1]) / denominator
-  ];
 }
 
 guyouRaw.invert = function(x, y) {
@@ -39,7 +22,7 @@ guyouRaw.invert = function(x, y) {
       K = ellipticF(halfPi, k * k),
       f = -1,
       j = ellipticJi(0.5 * K - y, -x, k * k),
-      tn = guyouComplexDivide(j[0], j[1]),
+      tn = complexDivide(j[0], j[1]),
       lambda = atan2(tn[1], tn[0]) / f;
   return [
     lambda,

a04cee2eb2d00a0bccb074feae0837799a20d061
Philippe Rivière <fil@rezo.net>
2017-07-11 07:06:17 +0200

    porting complexPow from Complex.js

--- src/complex.js
+++ src/complex.js
@@ -1,4 +1,4 @@
-import {atan2, halfPi, log} from "./math";
+import {abs, atan2, cos, exp, halfPi, log, pow, sin} from "./math";
 
 export function complexAtan(x, y) {
   var x2 = x * x,
@@ -46,9 +46,43 @@ export function complexNorm2(a) {
   return a[0] * a[0] + a[1] * a[1];
 }
 
+export function complexLogHypoth(a, b) {
+  var _a = abs(a),
+      _b = abs(b);
+  if (a === 0) return log(_b);
+  if (b === 0) return log(_a);
+  if (_a < 3000 && _b < 3000) return log(a * a + b * b) * 0.5;
+  return log(a / cos(atan2(b, a)));
+}
+
+// adapted from https://github.com/infusion/Complex.js
 export function complexPow(a, n) {
-  return [
-    a[0] - b[0],
-    a[1] - b[1]
-  ];
+  var b = a[1], arg, loh;
+  a = a[0];
+  if (a === 0 && b === 0) return [0,0];
+  
+  if (typeof n === 'number') n = [n,0];
+
+  if (!n[1]) {
+    if (b === 0 && a >= 0) {
+      return [pow(a, n[0]), 0];
+    } else if (a === 0) {
+      switch ((n[1] % 4 + 4) % 4) {
+        case 0:
+          return [pow(b, n[0]), 0];
+        case 1:
+          return [0, pow(b, n[0])];
+        case 2:
+          return [-pow(b, n[0]), 0];
+        case 3:
+          return [0, -pow(b, n[0])];
+        }
+      }
+    }
+
+    arg = atan2(b, a);
+    loh = complexLogHypot(a, b);
+    a = exp(n[0] * loh - n[1] * arg);
+    b = n[1] * loh + n[0] * arg;
+    return [a * cos(b), a * sin(b)];
 }
